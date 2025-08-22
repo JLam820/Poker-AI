dc9ce49863281d89cb9deca92f80b846b193c2b6
Chun Yin Lam <chun.lam-5@student.manchester.ac.uk>
2025-08-22 18:30:08 +0100

    Refactor probability initialization and input functions for hole and community cards

--- Evaluation.py
+++ Evaluation.py
@@ -3,8 +3,77 @@ cards = ["2d", "3d", "4d", "5d", "6d", "7d", "8d", "9d", "Td", "Jd", "Qd", "Kd",
          "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "Th", "Jh", "Qh", "Kh", "Ah",
          "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "Ts", "Js", "Qs", "Ks", "As"]
 
-def ResetProbabilities():
-    global HighCard, Pair, TwoPair, ThreeOfAKind, Straight, Flush, FullHouse, FourOfAKind, StraightFlush, RoyalFlush, CommunityCardStatus
+
+def HighCardDisplay():
+    pass
+
+
+def PairProbability():
+    pass
+
+
+def TwoPairProbability():
+    pass
+
+
+def ThreeOfAKindProbability():
+    pass
+
+
+def StraightProbability():
+    pass
+
+
+def FlushProbability():
+    pass
+
+
+def FullHouseProbability():
+    pass
+
+
+def FourOfAKindProbability():
+    pass
+
+
+def StraightFlushProbability():
+    pass
+
+
+def RoyalFlushProbability():
+    pass
+
+
+def ProbabilityDisplay():
+    pass
+
+
+def holeCardsInput(holeCards):
+
+    flag = True
+    while flag:
+        holeCards = input("Enter your hole cards: ").strip().split()
+        if len(holeCards) != 2 or any(card not in cards for card in holeCards):
+            print("Invalid input. Please enter two valid hole cards.")
+        else:
+            deck.remove(holeCards[0])
+            deck.remove(holeCards[1])
+            flag = False
+
+
+def communityCardsInput(communityCards):
+
+    flag = True
+    while flag:
+        communityCards = input("Enter the community cards: ").strip().split()
+        if len(communityCards) != 5 or any(card not in cards for card in communityCards):
+            print("Invalid input. Please enter five valid community cards.")
+        else:
+            for card in communityCards:
+                deck.remove(card)
+            flag = False
+
+while True:
     HighCard = ""
     Pair = 0
     TwoPair = 0
@@ -16,40 +85,17 @@ def ResetProbabilities():
     StraightFlush = 0
     RoyalFlush = 0
     CommunityCardStatus = 0
-
-def HighCardDisplay():
-    pass
-
-def PairProbability():
-    pass
-
-def TwoPairProbability():
-    pass
-
-def ThreeOfAKindProbability():
-    pass
-
-def StraightProbability():
-    pass
-
-def FlushProbability():
-    pass
-
-def FullHouseProbability():
-    pass
-
-def FourOfAKindProbability():
-    pass
-
-def StraightFlushProbability():
-    pass
-
-def RoyalFlushProbability():
-    pass
-
-def ProbabilityDisplay():
-    pass
+    holeCards = []
+    communityCards = []
+    deck = cards.copy()
 
 
-while True:
-    ResetProbabilities()
+    while CommunityCardStatus <= 3:
+        if CommunityCardStatus == 0:
+            holeCardsInput(holeCards)
+            ProbabilityDisplay()
+            CommunityCardStatus += 1
+        else:
+            communityCardsInput(communityCards)
+            ProbabilityDisplay()
+            CommunityCardStatus += 1
