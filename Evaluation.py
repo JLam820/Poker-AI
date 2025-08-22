9eadfff5ab327cfd8c245b843634f0d2e1e95ce6
Chun Yin Lam <chun.lam-5@student.manchester.ac.uk>
2025-08-22 18:34:08 +0100

    Enhance code structure by adding comments

--- Evaluation.py
+++ Evaluation.py
@@ -1,9 +1,10 @@
+# Card Deck -------------------------------------------------------------------------------------------------
 cards = ["2d", "3d", "4d", "5d", "6d", "7d", "8d", "9d", "Td", "Jd", "Qd", "Kd", "Ad",
          "2c", "3c", "4c", "5c", "6c", "7c", "8c", "9c", "Tc", "Jc", "Qc", "Kc", "Ac",
          "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "Th", "Jh", "Qh", "Kh", "Ah",
          "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "Ts", "Js", "Qs", "Ks", "As"]
 
-
+# Probability Functions -------------------------------------------------------------------------------------
 def HighCardDisplay():
     pass
 
@@ -44,10 +45,12 @@ def RoyalFlushProbability():
     pass
 
 
+# Probability Display ---------------------------------------------------------------------------------------
 def ProbabilityDisplay():
     pass
 
 
+# Card Input Functions --------------------------------------------------------------------------------------
 def holeCardsInput(holeCards):
 
     flag = True
@@ -73,6 +76,9 @@ def communityCardsInput(communityCards):
                 deck.remove(card)
             flag = False
 
+
+
+# Main Program Loop -----------------------------------------------------------------------------------------
 while True:
     HighCard = ""
     Pair = 0
